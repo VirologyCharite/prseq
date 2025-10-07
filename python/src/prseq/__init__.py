1dae738d682ff03c8a231d149023d017ade04241
terrycojones <tcj25@cam.ac.uk>
2025-10-07 12:41:32 +0200

    Use crates.io approved project categories. Move to 0.0.9

--- python/src/prseq/__init__.py
+++ python/src/prseq/__init__.py
@@ -1,7 +1,7 @@
 from .fasta import FastaRecord, FastaReader, read_fasta
 from .fastq import FastqRecord, FastqReader, read_fastq
 
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __all__ = ["FastaRecord", "FastaReader", "read_fasta", "FastqRecord", "FastqReader", "read_fastq"]
 
 
