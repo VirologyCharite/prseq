1110cb3449154f05d87880cc79ada6d76fca119b
terrycojones <tcj25@cam.ac.uk>
2025-10-12 14:15:29 +0200

    Bump version: 0.0.27 → 0.0.28

--- python/src/prseq/__init__.py
+++ python/src/prseq/__init__.py
@@ -1,7 +1,7 @@
 from .fasta import FastaReader, FastaRecord, read_fasta
 from .fastq import FastqReader, FastqRecord, read_fastq
 
-__version__ = "0.0.27"
+__version__ = "0.0.28"
 __all__ = [
     "FastaRecord",
     "FastaReader",
